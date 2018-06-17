1c412f4df74faafbaa19bb7761ce21304d194c3d
unknown <youssefswailem@gmail.com>
2018-06-17 21:34:55 +0200

    Update base model and add API function

--- src/features.py
+++ src/features.py
@@ -1,28 +1,39 @@
 import os
 import pickle
+import itertools
 import urllib.request
 import numpy as np
+from os.path import dirname, join
 
 
 def downloadimages(src, destination):
 
-    # download images given location of scraped.json
+    # download images given location of pickle dump
+
+    directory = dirname(__file__)
+    src = join(directory, src)
+    destination = join(directory, destination)
 
     if not os.path.exists(destination):
         os.makedirs(destination)
 
     data = pickle.load(open(src, 'rb'))
+    spinner = itertools.cycle(['.', '..', '...'])
     i = 0
+
     for _, pictures in data.items():
+        print('\rDownloading images{}'.format(next(spinner)), end="")
         for picture in pictures:
-            urllib.request.urlretrieve(
-                picture['img-small'], destination + 'img' + str(i) + '.jpeg')
+            urllib.request.urlretrieve(picture['img-small'],
+                                       destination + 'img' + str(i) + '.jpeg')
             i += 1
 
 
-def getlikes(src, destination):
+def getlikes(src):
 
-    # get number of likes for each image given  location of scraped.json
+    # get number of likes for each image given location of pickle dump
+    directory = dirname(__file__)
+    src = join(directory, src)
 
     data = pickle.load(open(src, 'rb'))
     likes = []
--- src/lipy.py
+++ src/lipy.py
@@ -1,6 +1,6 @@
 from features import downloadimages, getlikes
 from keras.models import Sequential
-from keras.layers import Conv2D, Activation, MaxPool2D, Flatten, Dense
+from keras.layers import Conv2D, Activation, MaxPool2D, Flatten, Dense, Dropout
 import numpy as np
 import cv2
 import glob
@@ -14,11 +14,10 @@ TRAININGPERCENT = 0.7
 
 # get likes array where Nth element corresponds to the number of likes
 # on image ../data/download/N.jpeg
-likes = getlikes(src='../data/pickledump',
-                 destination='../data/download/')
+likes = getlikes(src='../data/pickledump')
 
-likes = np.array(likes)
-likes = (likes - np.mean(likes))/np.std(likes)  # normalize
+likesraw = np.array(likes)
+likes = (likesraw - np.mean(likesraw))/np.std(likesraw)  # normalize
 
 images = []
 
@@ -39,24 +38,29 @@ y_train = likes[:training_num]
 x_test = images[training_num:]
 y_test = likes[training_num:]
 
-
 # create model and add layers
 model = Sequential()
 
-model.add(Conv2D(10, 3, 3, activation='relu',
+model.add(Conv2D(10, 5, 5, activation='relu',
                  input_shape=(IMAGESIZE[0], IMAGESIZE[1], 3)))
 
-model.add(Conv2D(10, 3, 3, activation='relu'))
+model.add(Conv2D(10, 5, 5, activation='relu'))
 model.add(MaxPool2D((5, 5)))
+model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(50))
 model.add(Activation('relu'))
 model.add(Dense(1))
 
 print(model.summary())
+
 model.compile(loss='mse',
               optimizer='rmsprop')
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train, epochs=10, shuffle=True,
+          validation_data=(x_test, y_test))
+
+model.save('../models/regv1.h')
+
 score = model.evaluate(x_test, y_test)
 print('test set mse is {}'.format(score))
